229bc4d5dc90ac31c6cc99283ab7053778dc8907
Nikolay Milovanov <nmil@itransformers.net>
2018-01-12 07:34:42 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-CLASSPATH=target/java-mrt-1.0.jar:~/.m2/repository/org/apache/commons/commons-compress/1.0/commons-compress-1.0.jar
+CLASSPATH=target/java-mrt-1.0-jar-with-dependencies.jar:
 JAVA_OPTS=
 java -Xms2024m -Xmx4048m  -cp $CLASSPATH org.javamrt.dumper.Route2GraphmlDumper $*
