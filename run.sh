2e3e0122090c6ac0c243ed720187299e11a88960
Nikolay Milovanov <nmil@itransformers.net>
2018-01-12 10:01:00 +0200

    Updating run.sh to run jar with deps

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-CLASSPATH=target/java-mrt-1.0-jar-with-dependencies.jar;
-JAVA_OPTS=
-java -Xms2024m -Xmx4048m  -cp $CLASSPATH org.javamrt.dumper.Route2GraphmlDumper $*
+CLASSPATH=./target/java-mrt-1.0-jar-with-dependencies.jar;
+JAVA_OPTS= -Xms2024m -Xmx4048m
+java $JAVA_OPTS -jar  $CLASSPATH  $*
